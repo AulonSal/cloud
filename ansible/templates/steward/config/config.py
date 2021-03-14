98a6401b2b2a9cf6543aa4066c18a13d25b623e2
AulonSal <aulonsal@gmail.com>
2021-03-14 13:56:43 +0530

    Generate files from templates

---
+++ ansible/templates/steward/config/config.py
@@ -0,0 +1,13 @@
+"""Some vars"""
+from os import environ
+
+IS_TEST = bool(environ.get("API_TEST"))
+
+POSTGRES_CREDENTIALS = {
+                    'user': '{{ steward_db_user }}',
+                    'password': '{{ steward_db_password }}',
+                    'host': '{{ steward_db_host }}',
+                    'database': '{{ steward_db_database }}',
+                    'port': {{ steward_db_port }},
+                }
+
--- ansible/templates/steward/run_prod.py
+++ ansible/templates/steward/run_prod.py
@@ -8,8 +8,8 @@ from main import app
 # Uvicorn does not expose a run_from_config to go along with uvicorn.config
 CONFIG = dict(
         app='main:app',  # app must be passed using import string to enable reload or workers
-        uds='/home/aulonsal/steward-fastapi/web.sock',
-        # workers=3,
+        uds="{{ steward_socket_path }}",
+        # workers=3, # Uvicorn does not suppont unix sockets with multiple workers + only 1 vCpu
         log_level='info',
     )
 
