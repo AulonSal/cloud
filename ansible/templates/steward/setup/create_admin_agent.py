d50376acde0ea98573d71d297a9fc4153f86a6d9
AulonSal <aulonsal@gmail.com>
2021-03-17 19:41:43 +0530

    Create admin agent when deploying

---
+++ ansible/templates/steward/setup/create_admin_agent.py
@@ -0,0 +1,17 @@
+from steward_fastapi.core.authentication import create_new_active_agent
+from steward_fastapi.utils import TortoiseContext
+import asyncio
+
+
+async def main():
+    async with TortoiseContext():
+        await create_new_active_agent(
+            username="{{ steward_api_admin_agent }}",
+            password="{{ steward_api_admin_password }}",
+        )
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
