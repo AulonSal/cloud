034435f14a11acb922d57d46b1d92ee4ff8b8a40
AulonSal <aulonsal@gmail.com>
2021-03-28 07:12:15 +0530

    Close db connections before deleting it

--- ansible/templates/scripts/refresh_database.sql
+++ ansible/templates/scripts/refresh_database.sql
@@ -1,3 +1,6 @@
+-- first drop all connections to the database
+select pg_terminate_backend(pid) from pg_stat_activity where datname="{{ steward_db_database }}";
+
 \i drop_database.sql
 \i create_database.sql
 
