0a512958d50e47aff91f84323598c8db7060eb46
AulonSal <aulonsal@gmail.com>
2021-03-29 02:36:59 +0530

    Add initial rules for steward-discord

---
+++ ansible/templates/steward-discord/steward_discord/secrets.py
@@ -0,0 +1,4 @@
+token = "{{ steward_discord_token }}"
+USERNAME = "{{ steward_api_discord_agent }}"
+PASSWORD = "{{ steward_api_discord_password }}"
+
--- ansible/templates/steward/setup/create_admin_agent.py
+++ ansible/templates/steward/setup/create_admin_agent.py
@@ -9,6 +9,14 @@ async def main():
             username="{{ steward_api_admin_agent }}",
             password="{{ steward_api_admin_password }}",
         )
+        await create_new_active_agent(
+            username="{{ steward_api_discord_test_agent }}",
+            password="{{ steward_api_discord_test_password }}",
+        )
+        await create_new_active_agent(
+            username="{{ steward_api_discord_agent }}",
+            password="{{ steward_api_discord_password }}",
+        )
 
 
 if __name__ == '__main__':
